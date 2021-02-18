076771bcb50fb37ad043497a69e1e2c2b01fcfbc
Camilo Caceres <ca.cceres3@gmail.com>
2021-02-18 15:10:12 +0100

    add grid

--- voxeliser/main.cpp
+++ voxeliser/main.cpp
@@ -3,9 +3,12 @@
 #include <vector>
 #include <sstream>
 
+#include <math.h> 
+
 #include "Point.h"
 #include "Rows.h"
 #include "VoxelGrid.h"
+
 
 float signed_volume(const Point &a, const Point &b, const Point &c, const Point &d) {
   // to do
@@ -17,18 +20,9 @@ bool intersects(const Point &orig, const Point &dest, const Point &v0, const Poi
   return 0;
 }
 
-int main(int argc, const char * argv[]) {
-  const char *file_in = "bag_bk.obj";
-  const char *file_out = "vox.obj";
-  float voxel_size = 1.0;
-
-  // Read file
-  std::vector<Point> vertices;
-  std::vector<std::vector<unsigned int>> faces;
-  // to do
-  std:: string  input =  file_in;
-  input = "../" + input;
-  std::cout << "Reading file: " << file_in << std::endl;
+int read_object(std::string  input,   std::vector<Point> &vertices, std::vector<std::vector<unsigned int>> &faces)
+{
+  std::cout << "Reading file: " << input << std::endl;
   std::ifstream infile(input.c_str(), std::ifstream::in);
   if (!infile)
   {
@@ -95,12 +89,71 @@ int main(int argc, const char * argv[]) {
   }
   std::cout << "Number of vertices: " << vertices.size() << std:: endl;
   std::cout << "Number of faces: " << faces.size() << std:: endl;
+  return 0;
+}
 
+void boundary_box(std::vector<Point> &boundary,std::vector<Point> &vertices )
+{
+  Point min_pt = vertices[0], max_pt = vertices[0];
+  for (int i = 0 ; i < vertices.size(); i++)
+  {
+    if(vertices[i].x < min_pt.x)
+    {
+      min_pt.x = vertices[i].x;
+    }
+    if(vertices[i].y < min_pt.y)
+    {
+      min_pt.y = vertices[i].y;
+    }
+    if(vertices[i].z < min_pt.z)
+    {
+      min_pt.z = vertices[i].z;
+    }
+    if(vertices[i].x > max_pt.x)
+    {
+      max_pt.x = vertices[i].x;
+    }
+    if(vertices[i].y > max_pt.y)
+    {
+      max_pt.y = vertices[i].y;
+    }
+    if(vertices[i].z > max_pt.z)
+    {
+      max_pt.z = vertices[i].z;
+    }
+  }
+  boundary.push_back(min_pt);
+  boundary.push_back(max_pt);
+}
 
+int main(int argc, const char * argv[]) {
+  const char *file_in = "bag_bk.obj";
+  const char *file_out = "vox.obj";
+  float voxel_size = 1.0;
+
+  // Read file
+  std::vector<Point> vertices;
+  std::vector<std::vector<unsigned int>> faces;
+  // to do
+  std:: string  input =  file_in;
+  input = "../" + input;
+  //READ FILE
+  read_object(input, vertices, faces);
   // Create grid
-  Rows rows;
+  std::vector<Point> boundary;
+  boundary_box(boundary, vertices);
+  std::cout << "Boundary points min :" << boundary[0].x << "," << boundary[0].y << "," << boundary[0].z << std::endl;
+  std::cout << "Boundary points max :" << boundary[1].x << "," << boundary[1].y << "," << boundary[1].z << std::endl;
+  
+  int row_x = std::ceil((boundary[1].x - boundary[0].x)/voxel_size);
+  int row_y = std::ceil((boundary[1].y - boundary[0].y)/voxel_size);
+  int row_z = std::ceil((boundary[1].z - boundary[0].z)/voxel_size);
+
+  Rows rows(row_x, row_y, row_z);
+
   // to do
   VoxelGrid voxels(rows.x, rows.y, rows.z);
+
   
   // Voxelise
   for (auto const &triangle: faces) {
